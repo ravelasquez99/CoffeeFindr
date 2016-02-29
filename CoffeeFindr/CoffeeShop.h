923d317cc10172c8a58123eadfe29877c1bd0da8
ravelasquez99 <ricky.wedew@gmail.com>
2016-02-29 14:00:49 -0800

    NSLog five closets coffee shops

---
+++ CoffeeFindr/CoffeeShop.h
@@ -0,0 +1,13 @@
+//
+//  CoffeeShop.h
+//  CoffeeFindr
+//
+//  Created by Richard Velazquez on 2/29/16.
+//  Copyright © 2016 Richard Velazquez. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface CoffeeShop : NSObject
+
+@end
---
+++ CoffeeFindr/CoffeeShop.m
@@ -0,0 +1,13 @@
+//
+//  CoffeeShop.m
+//  CoffeeFindr
+//
+//  Created by Richard Velazquez on 2/29/16.
+//  Copyright © 2016 Richard Velazquez. All rights reserved.
+//
+
+#import "CoffeeShop.h"
+
+@implementation CoffeeShop
+
+@end
